6d682271e47cee87f9807bb933306f6944569fed
Luciano Morandi <luciano.morandi@gmail.com>
2025-03-08 12:11:12 -0300

    add form to book a ticket

--- Dev-Test-LucianoMorandi/src/App.jsx
+++ Dev-Test-LucianoMorandi/src/App.jsx
@@ -7,13 +7,15 @@ const API_URL_EVENTS = 'https://goldfish-app-fbulw.ondigitalocean.app/Event?appl
 function App() {
   const [event, setEvent] = useState([])
   const [eventInfo, setEventInfo] = useState(null)
+  const [buyButtonSate, setBuyButtonSate] = useState(null)
+  const [buyForm, setBuyForm] = useState(null)
 
   useEffect(() => {
     fetch(API_URL_EVENTS)
-     .then(res => res.json())
-     .then(data => {
-      setEvent(data)
-     })
+      .then(res => res.json())
+      .then(data => {
+        setEvent(data)
+      })
 
   }, [])
 
@@ -21,39 +23,72 @@ function App() {
     setEventInfo(true)
   }
 
-  const handleClick = () => {
+  const eventClick = () => {
     getInfoEvent()
-  }     
+    setBuyButtonSate(true)
+  }
 
   const backClick = () => {
     setEventInfo(null)
+    setBuyForm(null)
   }
+
+  const buyButton = () => {
+    setBuyButtonSate(null)
+    setBuyForm(true)
+  }
+
+  const CompleteBuyButton = () => {
+    setEventInfo(null)
+    setBuyForm(null)
+  }
+
+  const cancelButton = () => {
+    setBuyForm(null)
+    setBuyButtonSate(true)
+
+  }
+
+
+
   return (
     <>
       <h1>Dev Test</h1>
       <div >
-      <ul className='event-container' >{event.map(item => (
-        <li className='event' key={item.id}>Nombre del evento:{item.name}
-         Fecha del evento: {item.date}
-         Precio: {item.price}
-         Available seats: 5
-         <button onClick={handleClick}>Event details</button></li>
-      ))}</ul>
+        <ul className='event-card' >{event.map(item => (
+          <li className='event' key={item.id}>Nombre del evento:{item.name}
+            Fecha del evento: {item.date}
+            Precio: {item.price}
+            Available seats: 5
+            <button onClick={eventClick}>Event details</button></li>
+        ))}</ul>
 
-      {eventInfo && 
-        <div>
-          <button onClick={backClick}>back</button>
-          <h2>Event Details</h2>
-          <p>Event: {event.name}</p>
-          <p>Date: {event.date}</p>
-          <p>Location: this is the event location</p>
-          <strong>This is the event description</strong>
-          <p>Ticket price: {event.price}</p>
-          <p>Available seats: 5</p>
-          <button>Book Ticket</button>
+        <div className='event-details'>
+          {eventInfo &&
+            <div>
+              <button onClick={backClick}>back</button>
+              <h2>Event Details</h2>
+              <p>Event: {event.name}</p>
+              <p>Date: {event.date}</p>
+              <p>Location: this is the event location</p>
+              <strong>This is the event description</strong>
+              <p>Ticket price: {event.price}</p>
+              <p>Available seats: 5</p>
+              {buyButtonSate && <button onClick={buyButton}>Book Ticket</button>}
+            </div>
+          }
+          {buyForm &&
+            <form action="">
+              <label for='name'>Customer Name</label>
+              <input type="text" id='name' />
+              <label for='quantity'>Tickets to Book</label>
+              <input type="number" id='quantity' />
+              <p>Total Price: $</p>
+              <button onClick={CompleteBuyButton}>Book now</button>
+              <button onClick={cancelButton}>Cancel</button>
+            </form>
+          }
         </div>
-      }
-
       </div>
     </>
   )
